565ceddcf5b2639f9c3317774e25d89f8f2e6286
Chinedum Echeta <cecheta@deloitte.co.uk>
2020-08-21 23:20:10 +0100

    Finished move logic for pawn

--- src/js/models/Pawn.js
+++ src/js/models/Pawn.js
@@ -5,18 +5,36 @@ export default class Pawn extends Piece {
     super(square, player);
   }
 
-  possibleMoves(square) {
+  possibleMoves(square, state) {
+    const currentLetter = square.charAt(0);
+    const currentDigit = parseInt(square.charAt(1));
     const possibleSquares = [];
-    const letter = square.charAt(0);
-    const digit = parseInt(square.charAt(1));
+    let verticalSquares, diagonalSquares;
 
-    if (digit <= 7) {
-      possibleSquares.push(`${letter}${digit + 1}`);
+    if (state.player === 1) {
+      verticalSquares = [`${currentLetter}${currentDigit + 1}`, `${currentLetter}${currentDigit + 2}`];
+      diagonalSquares = [`${String.fromCharCode(currentLetter.charCodeAt(0) + 1)}${currentDigit + 1}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 1)}${currentDigit + 1}`];
+    } else {
+      verticalSquares = [`${currentLetter}${currentDigit - 1}`, `${currentLetter}${currentDigit - 2}`];
+      diagonalSquares = [`${String.fromCharCode(currentLetter.charCodeAt(0) + 1)}${currentDigit - 1}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 1)}${currentDigit - 1}`];
     }
 
-    if (!this.hasMoved && digit <= 6) {
-      possibleSquares.push(`${letter}${digit + 2}`);
+    const firstSquare = state.squares.find((el) => el.id === verticalSquares[0]);
+    if (firstSquare && !firstSquare.piece) {
+      possibleSquares.push(verticalSquares[0]);
+
+      const secondSquare = state.squares.find((el) => el.id === verticalSquares[1]);
+      if (secondSquare && !secondSquare.piece && !this.hasMoved) {
+        possibleSquares.push(verticalSquares[1]);
+      }
     }
+
+    diagonalSquares.forEach((square) => {
+      const diagonalSquare = state.squares.find((el) => el.id === square);
+      if (diagonalSquare && diagonalSquare.piece && diagonalSquare.piece.player !== state.player) {
+        possibleSquares.push(square);
+      }
+    });
 
     return possibleSquares;
   }
