26263f199a57ef53450b405ba681ef2087fcc390
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2020-08-21 11:49:55 +0100

    Added pieces and squares models

---
+++ src/js/models/Bishop.js
@@ -0,0 +1,7 @@
+import Piece from './Piece';
+
+export default class Bishop extends Piece {
+  constructor(id, player) {
+    super(id, player);
+  }
+}
---
+++ src/js/models/Queen.js
@@ -0,0 +1,7 @@
+import Piece from './Piece';
+
+export default class Queen extends Piece {
+  constructor(id, player) {
+    super(id, player);
+  }
+}
