47d26102b2f5a7d4d2768425f996d8f52cd83eb4
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2020-08-22 17:04:40 +0100

    Removed rendering dots (temp) Added getAttackedSquares to Pawn

--- src/js/models/Pawn.js
+++ src/js/models/Pawn.js
@@ -1,8 +1,23 @@
 import Piece from './Piece';
+import * as utils from '../utils';
 
 export default class Pawn extends Piece {
   constructor(square, player) {
     super(square, player);
+  }
+
+  getAttackedSquares(allSquares) {
+    const currentSquare = this.getSquare(allSquares).id;
+    const currentLetter = currentSquare.charAt(0);
+    const currentDigit = parseInt(currentSquare.charAt(1));
+
+    let attackedSquares;
+    if (this.player === 1) {
+      attackedSquares = [`${String.fromCharCode(currentLetter.charCodeAt(0) + 1)}${currentDigit + 1}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 1)}${currentDigit + 1}`];
+    } else {
+      attackedSquares = [`${String.fromCharCode(currentLetter.charCodeAt(0) + 1)}${currentDigit - 1}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 1)}${currentDigit - 1}`];
+    }
+    return attackedSquares.filter((id) => !!utils.getSquare(id, allSquares));
   }
 
   possibleMoves(square, state) {
