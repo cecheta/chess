a1ba0bd063bf6c7b493b8fa0bd1064ba45fde48d
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2020-08-22 18:47:11 +0100

    Added getAttackingSquares functions

--- src/js/models/Knight.js
+++ src/js/models/Knight.js
@@ -1,7 +1,18 @@
 import Piece from './Piece';
+import * as utils from '../utils';
 
 export default class Knight extends Piece {
   constructor(square, player) {
     super(square, player);
+  }
+
+  getAttackedSquares(allSquares) {
+    const currentSquare = this.getSquare(allSquares).id;
+    const currentLetter = currentSquare.charAt(0);
+    const currentDigit = parseInt(currentSquare.charAt(1));
+
+    const attackedSquares = [`${String.fromCharCode(currentLetter.charCodeAt(0) + 2)}${currentDigit + 1}`, `${String.fromCharCode(currentLetter.charCodeAt(0) + 2)}${currentDigit - 1}`, `${String.fromCharCode(currentLetter.charCodeAt(0) + 1)}${currentDigit + 2}`, `${String.fromCharCode(currentLetter.charCodeAt(0) + 1)}${currentDigit - 2}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 1)}${currentDigit + 2}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 1)}${currentDigit - 2}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 2)}${currentDigit + 1}`, `${String.fromCharCode(currentLetter.charCodeAt(0) - 2)}${currentDigit - 1}`];
+
+    return attackedSquares.filter((id) => !!utils.getSquare(id, allSquares));
   }
 }
