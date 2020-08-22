c1d425f995d144d65060a3fd8fb035166a83245d
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2020-08-22 20:53:55 +0100

    Improved rendering possible moves to include King check

--- src/js/utils.js
+++ src/js/utils.js
@@ -81,3 +81,46 @@ export function getAttackingSquaresDown(currentLetter, currentDigit, allSquares)
 export function getAttackingSquaresLeft(currentLetter, currentDigit, allSquares) {
   return getAttackingSquaresGeneric(currentLetter, currentDigit, 'minus', 'zero', allSquares);
 }
+
+export function getAllAttackedSquares(state) {
+  const opponentPieces = state.pieces.filter((piece) => piece.player !== state.player);
+  const allAttackedSquares = [];
+  opponentPieces.forEach((piece) => {
+    allAttackedSquares.push(...piece.getAttackedSquares(state.squares));
+  });
+  return Array.from(new Set(allAttackedSquares));
+}
+
+export function checkSquare(state, piece, newSquareId) {
+  const oldSquare = piece.getSquare(state.squares);
+  const newSquare = getSquare(newSquareId, state.squares);
+
+  let removedPiece;
+  if (newSquare.piece) {
+    const pieceIndex = state.pieces.indexOf(newSquare.piece);
+    removedPiece = state.pieces.splice(pieceIndex, 1)[0];
+  }
+
+  let tempPiece = null;
+  if (newSquare.piece) {
+    tempPiece = newSquare.piece;
+  }
+  newSquare.piece = oldSquare.piece;
+  oldSquare.piece = null;
+
+  const attackedSquares = getAllAttackedSquares(state);
+  const king = state.pieces.find((piece) => piece.player === state.player && piece.constructor.name === 'King');
+  const kingId = king.getSquare(state.squares).id;
+  let validMove = true;
+  if (attackedSquares.find((square) => square === kingId)) {
+    validMove = false;
+  }
+
+  if (removedPiece) {
+    state.pieces.push(removedPiece);
+  }
+  oldSquare.piece = newSquare.piece;
+  newSquare.piece = tempPiece;
+
+  return validMove;
+}
